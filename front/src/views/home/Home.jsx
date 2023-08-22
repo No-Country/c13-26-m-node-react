a24b88b248159de4c53cb6cf094137cdc87bd1c1
Yarumis <yarumisginanrm@gmail.com>
2023-08-21 23:59:57 -0400

    feat: segunda section de home agregada

--- front/src/views/home/Home.jsx
+++ front/src/views/home/Home.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
+import { FaBoxOpen } from "react-icons/fa6";
+import {FaCrown} from "react-icons/fa6";
+import {FaShieldHeart} from "react-icons/fa6";
 
 const Home = () => {
   return (
     <div className='h-screen my-12'>
       <Hero/>
-      <div className='bg-white border-solid border border-current rounded-xl h-16 sm:h-24 sm:m-12 m-6'>
-        Hola
+      <div className='bg-white flex items-center justify-center border-solid border border-current rounded-xl h-16 sm:h-32 sm:m-12 m-6'>
+        <div className='h-8 w-20 m-3 sm:h-20 sm:w-72 flex justify-center items-center sm:me-16'>
+          <FaBoxOpen className='h-6 w-8 sm:h-10 sm:w-12 m-1 sm:ms-5'/>
+          <div className='w-32 h-8 sm:h-12 sm:w-48 my-2 mx-1 flex flex-col justify-center'>
+            <p className='text-[8px] sm:text-xl font-medium'>Free delivery</p>
+            <p className='text-[6px] sm:text-sm'>on order above $50,00</p>
+          </div>
+        </div>
+        <div className='h-8 w-20 m-3 sm:h-20 sm:w-72 flex justify-center items-center'>
+          <FaCrown className='h-6 w-8 sm:h-10 sm:w-12 m-1 sm:ms-5'/>
+          <div className='w-32 h-8 sm:h-12 sm:w-48 my-2 mx-1 flex flex-col justify-center'>
+            <p className='text-[8px] sm:text-xl font-medium'>Best quality </p>
+            <p className='text-[6px] sm:text-sm'>best quality in low price</p>
+          </div>
+        </div>
+        <div className='h-8 w-20 m-3 sm:h-20 sm:w-72 flex justify-center items-center sm:ms-16'>
+          <FaShieldHeart className='h-6 w-8 sm:h-10 sm:w-12 m-1 sm:ms-5'/>
+          <div className='w-32 h-8 sm:h-12 sm:w-48 my-2 mx-1 flex flex-col justify-center'>
+            <p className='text-[8px] sm:text-xl font-medium'>1 year warranty</p>
+            <p className='text-[6px] sm:text-sm'>Avaliable warranty</p>
+          </div>
+        </div>
       </div>
     </div>
   )
