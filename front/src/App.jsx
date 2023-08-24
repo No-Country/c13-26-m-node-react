9558ad64290650a3bada59748a734c2f426566b5
nicoflorentin <nicoflorentn@gmail.com>
2023-08-24 00:49:46 -0300

    add router and cart view/components

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Home from './views/home/Home'
+import "./App.css"
+import Home from "./views/home/Home"
+import Cart from "./views/cart/Cart"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-
-  return (
-    <>
-      <Home/>
-    </>
-  )
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<Home/>}/>
+				<Route path='/cart' element={<Cart/>}/>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
---
+++ front/src/components/cart/CartTable/CartTable.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+
+let initial = [
+	{ itemName: "This is the longest longest longest item name", price: 1000, quantity: 3, subtotal: 3000 },
+	{ itemName: "Item Name", price: 1000, quantity: 3, subtotal: 3000 },
+	{ itemName: "Item Name", price: 1000, quantity: 3, subtotal: 3000 },
+]
+
+const CartTable = () => {
+	const [products, setProducts] = useState(initial)
+
+	return (
+		<table class="table-auto w-3/4 m-auto text-center">
+			<thead>
+				<tr className="bg-red-500 text-slate-50">
+					<th className="w-3/5 p-3 text-center rounded-tl-2xl">Item</th>
+					<th>Price</th>
+					<th>Quantity</th>
+					<th className="rounded-tr-2xl">Subtotal</th>
+				</tr>
+			</thead>
+			<tbody>
+				{products.map(product => 
+					<tr>
+						<td className="text-left p-3 flex"><button className="mx-5">x</button><p className="px-8">{product.itemName}</p></td>
+						<td>{product.price}</td>
+						<td>{product.quantity}</td>
+						<td>{product.subtotal}</td>
+					</tr>
+				)}
+			</tbody>
+		</table>
+	)
+}
+
+export default CartTable
