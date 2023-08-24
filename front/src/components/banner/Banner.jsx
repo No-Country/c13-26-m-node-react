4b6a69e621f1a296e94d88dfc4848d32736e021c
Yarumis <yarumisginanrm@gmail.com>
2023-08-24 02:35:13 -0400

    feat: realizada maquetacion de section de testimonios

--- front/src/components/banner/Banner.jsx
+++ front/src/components/banner/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div className="m-5 sm:m-10 rounded-xl bg-cover bg-center flex items-center justify-end h-36 sm:h-96 bg-[url('https://p1.pxfuel.com/preview/918/349/310/office-work-business-workspace.jpg')]">
-            <div className='text-center me-8 sm:me-28'>
-                <p className='text-[12px] sm:text-[40px] font-medium text-white'>¡Publica tus productos!</p>
-                <p className='text-[8px] sm:text-[20px] text-white'>Únete como vendedor</p>
-                <button class="bg-black hover:bg-red-500 text-white font-bold py-1 px-2 sm:px-5 sm:py-2 my-2 sm:text-[20px] text-[8px] rounded-full">
-                    Button
+        <div className="m-5 sm:m-10 rounded-xl sm:bg-cover bg-center flex items-center justify-end h-48 sm:h-96 bg-[url('https://p1.pxfuel.com/preview/918/349/310/office-work-business-workspace.jpg')]">
+            <div className='text-center me-[5%] sm:me-[10%]'>
+                <p className='text-lg sm:text-[40px] sm:mb-4 font-medium text-white'>¡Publica tus productos!</p>
+                <p className='text-sm sm:text-[20px] text-white'>Únete como vendedor</p>
+                <button class="bg-black hover:bg-red-500 text-white font-medium py-1 px-2 sm:px-5 sm:py-2 my-2 sm:text-[20px] text-sm rounded-full">
+                    Sign in
                 </button>
             </div>
         </div>
