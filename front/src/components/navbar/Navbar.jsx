4f191a164c9452fc3ee8db03ec07ca7ae9c87e0a
juanpablo1978 <jpandrujovich@gmail.com>
2023-08-28 10:05:38 -0300

    style: ajustes de iconos user y cart

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     
     <header className='bg-white text-black h-full w-full px-6 py-8 font-poppins'>
-      <article className='h-full flex items-center justify-center gap-11 pb-5'>
+      <article className='h-full flex items-center justify-center gap-11 pb-7'>
         <button className=''
         >
           <p className=' font-bold
@@ -25,7 +25,7 @@ const Navbar = () => {
     >
       <input
         type="text"
-        className="text-lg  md:w-[550px] bg-transparent border-[2px] h-[51px] border-red-600 
+        className="text-lg  md:w-[560px] bg-transparent border-[2px] h-[51px] border-red-600 
         outline-none rounded-3xl px-5"
         placeholder="Search for products"
         name='search'
@@ -35,12 +35,22 @@ const Navbar = () => {
         </button>
     </form>
       <nav>
-        <ul className='flex gap-5 font-semibold text-black text-[42px]'>
+        <ul className='flex gap-5 font-semibold text-black text-[42px] pt-4'>
           <li>
-            <AiOutlineUser/>
+            <button><AiOutlineUser/></button>
           </li>
           <li>
-            <BsCartDash/>
+            <div className='relative flex gap-3'>
+            <button><BsCartDash/></button>
+            <div className='rounded-full bg-red-600 text-white text-xs w-5 h-5 absolute
+              bottom-[25px] left-[25px]'>
+              <p className='pl-[7px] pt-[2px]'>0</p>
+              </div>
+              <div className='text-xs text-gray-500'>
+              <p>Tu carrito</p>
+              <p className='text-xl'>$ 0.00</p>
+            </div>
+              </div>
           </li>
         </ul>
       </nav>
@@ -49,7 +59,7 @@ const Navbar = () => {
       <article className='flex flex-row justify-center gap-11'>
         
         <button className='flex items-center border-[1px] border-gray-300 rounded-3xl
-        justify-center w-[250px] h-12 font-light text-gray-800 gap-16'>
+        justify-center w-[260px] h-12 font-light text-gray-800 gap-[75px]'>
           <div className='flex items-center gap-3'>
           <AiOutlineMenu/>
           <h2>
