18cd221e4610a6d4c66f7d32306d9f5b82ae29e0
juanpablo1978 <jpandrujovich@gmail.com>
2023-09-11 10:03:45 -0300

    style: cambio estilo navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
 					<div className="md:pt-3 font-semibold text-gray-700 ">
 						<ul className="md:flex gap-7 sm: hidden">
 							<li className="hover:text-red-600"><Link to='/'>Home</Link></li>
-							<li className="hover:text-red-600"><Link to='/cart'>Cart</Link></li>
+							{/*<li className="hover:text-red-600"><Link to='/cart'>Cart</Link></li>*/}
 							<li className="hover:text-red-600"><Link to='/detail'>Detail</Link></li>
 							<li className="hover:text-red-600"><Link to='/shop'>Shop</Link></li>
 							<li className="hover:text-red-600"><Link to='/login'>Login</Link></li>
