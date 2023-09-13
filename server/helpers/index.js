3b3d8810274956a6545779f819ebdca1e0090d53
Enmanuel Rodriguez <enmanuelrodmac@gmail.com>
2023-09-13 15:04:02 -0400

    feat: crear gestor productos

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -2,10 +2,11 @@
 
 const dbValidators = require('./db-validators');
 const generateJWT   = require('./generate-JWT');
-
+const uploadFileToServer = require('./upload-file');
 
 
 module.exports = {
     ...dbValidators,
     ...generateJWT,
+    ...uploadFileToServer,
 }
--- server/models/server.js
+++ server/models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
 
@@ -10,56 +11,60 @@ class Server {
         this.port = process.env.PORT;
 
         this.paths = {
-            auth:   '/api/auth',
+            auth:       '/api/auth',
+            search:     '/api/search',
             categories: '/api/categories',
-            roles:   '/api/roles',
-            users:   '/api/users',
+            products:   '/api/products',
+            users:      '/api/users',
+            uploads:    '/api/uploads',
         }
 
-
-        // Conectar a base de datos
-        this.dbConnect();
+        // Connect to the database
+        this.connectDB();
 
         // Middlewares
         this.middlewares();
 
-        // Rutas de mi aplicación
+        // Application routes
         this.routes();
     }
 
-    async dbConnect() {
+    async connectDB() {
         await dbConnection();
     }
 
-
     middlewares() {
-
         // CORS
         this.app.use( cors() );
 
-        // Lectura y parseo del body
+        // Reading and parsing the body
         this.app.use( express.json() );
 
+        // Public directory
+        this.app.use( express.static('public') );
+
+        // File upload - Uploading files
+        this.app.use( fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes() {
-        
-        this.app.use( this.paths.auth, require( '../routes/auth' ));
-        this.app.use(this.paths.categories, require('../routes/categories'));
-        this.app.use(this.paths.roles, require(  '../routes/roles' ));
-        this.app.use( this.paths.users, require( '../routes/users' ));
-        
+        this.app.use( this.paths.auth, require('../routes/auth'));
+        this.app.use( this.paths.search, require('../routes/search'));
+        this.app.use( this.paths.categories, require('../routes/categories'));
+        this.app.use( this.paths.products, require('../routes/products'));
+        this.app.use( this.paths.users, require('../routes/users'));
+        this.app.use( this.paths.uploads, require('../routes/uploads'));
     }
 
     listen() {
         this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port );
+            console.log('Server is running on port', this.port );
         });
     }
-
 }
-
-
-
 
 module.exports = Server;
