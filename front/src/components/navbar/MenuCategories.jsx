1a47403ecba16a88ca62fcd7e56c4953606df495
juanpablo1978 <jpandrujovich@gmail.com>
2023-09-14 10:25:17 -0300

    feat: pruba pull

--- front/src/components/navbar/MenuCategories.jsx
+++ front/src/components/navbar/MenuCategories.jsx
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom"
 
 
 const MenuCategories = () => {
   return (
-    <article className=" font-semibold text-[14px] font-poppins text-gray-700 leading-10 md:w-[260px]
-     md:border-gray-700 md:border-2 md:pl-4 md:leading-[50px] md:rounded-xl">
-        <ul>
-            <li className="text-red-600 md:border-b-[1px] border-gray-300">Cameras</li>
-            <li className="md:border-b-[1px] border-gray-300">Cell Phones</li>
-            <li className="md:border-b-[1px] border-gray-300">Computer Hardware</li>
-            <li className="md:border-b-[1px] border-gray-300">Headphones</li>
-            <li className="md:border-b-[1px] border-gray-300">Laptops</li>
-            <li className="md:border-b-[1px] border-gray-300">Monitors</li>
-            <li className="md:border-b-[1px] border-gray-300">Smart Watches</li>
-            <li className="md:border-b-[1px] border-gray-300">Tablets</li>
-            <li className="md:border-b-[1px] border-gray-300">Televisions</li>
-            <li className="md:border-b-[1px] border-gray-300">Video Games</li>
+    <article className="bg-black w-52 pl-3 font-semibold text-[14px] font-poppins text-white
+     leading-10 md:w-[250px]
+     border-gray-700 border-2 md:pl-4 md:leading-[50px] rounded-xl">
+        <ul>      
+            <li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Cameras</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Cell Phones</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Computer Hardware</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Headphones</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Laptops</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Monitors</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Smart Watches</Link></li>
+            <li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Tablets</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Televisions</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Video games</Link></li>
           </ul>
     </article>
   )
--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -3,46 +3,79 @@ import { BsCartDash } from "react-icons/bs"
 import { AiOutlineUser, AiOutlineMenu } from "react-icons/ai"
 import { BiChevronDown } from "react-icons/bi"
 import { FiSearch } from "react-icons/fi"
-import MenuCategories from "./MenuCategories"
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import MenuCategories from "./MenuCategories"
+import Swal from "sweetalert2"
+import { useNavigate } from 'react-router-dom';
+
 
 
 const Navbar = () => {
+
+	const navigate = useNavigate();  
+
+	const handleSubmit = (e) => {
+        e.preventDefault();
+		const search = e.target.search.value.trim();
+		console.log(search);
+
+		if (search.length === 0) {
+            Swal.fire('Ingrese un producto');
+        } else if (search.length <4) {
+          Swal.fire('Ingrese cuatro caracteres');
+        }else{
+          e.target.search.value = '';
+          navigate(`/results?search=${search}`);
+        }
+	}
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleOpen = () => {
+
+		setIsOpen(!isOpen);
+	}
+
+	
+
 	return (
-		<header className="bg-white text-black h-full w-full px-6 py-8 font-poppins">
+
+		<header className="bg-black text-white h-full w-full px-6 py-12 font-poppins border-b-[1px]
+		 border-gray-400">
 			<article className="h-full flex items-center justify-center gap-11 pb-9 flex-wrap">
-				<div className="">
-					<button className="">
+				<div>
+					<button>
 						<p
 							className=" font-bold
-          text-4xl pt-[4px] pl-[2px] text-black"
+          text-4xl pt-[4px] pl-[2px] text-white"
 						>
-							E-COMMERCE
+						<Link to='/'>E-TECH</Link>	
 						</p>
 					</button>
 				</div>
 
 				<div className="flex flex-row items-center gap-56 md:gap-10">
 					<form
-						className="flex
-        gap-2 text-white text-2xl pr-5 relative"
+						className="flex text-white
+        gap-2  text-2xl pr-5 relative" onSubmit={handleSubmit}
 					>
 						<input
 							type="text"
-							className="text-lg md:w-[560px] bg-transparent border-[2px] md:h-[51px] border-red-600 
+							className="text-lg md:w-[560px] bg-transparent border-[2px] md:h-[51px] border-gray-300 
         outline-none rounded-3xl px-5 sm: w-[50px] h-[15px] hidden md:block"
 							placeholder="Search for products"
 							name="search"
 						/>
 						<div className="absolute text-xl md:left-[500px] md:bottom-2">
 							<button type="submit">
-								<FiSearch className="text-gray-900" />
+								<FiSearch className="text-white" />
 							</button>
 						</div>
 					</form>
 
 					<nav>
-						<ul className="flex gap-5 font-semibold text-black text-[32px] pt-6 md:text-[42px]">
+						<ul className="flex gap-5 font-semibold text-white text-[32px] pt-6 md:text-[42px] md: pb-4">
 							<li>
 								<Link to="/userDashboard">
 									<AiOutlineUser />
@@ -56,7 +89,7 @@ const Navbar = () => {
 										</button>
 										<div
 											className="rounded-full  bg-red-600 text-white text-xs w-5 h-5 absolute
-              bottom-[26px] left-[25px]"
+                                            bottom-[26px] left-[25px]"
 										>
 											<p className="pl-[7px] pt-[2px]">0</p>
 										</div>
@@ -72,18 +105,21 @@ const Navbar = () => {
 				</div>
 			</article>
 
-			<article className="flex flex-row justify-center h-[2px] gap-5 items-center md:gap-11">
+			<article className="flex flex-row justify-center h-[2px] gap-5 items-center md:gap-11 relative">
 				<div className="flex items-center md:gap-8">
 					<button
 						className="md:flex items-center md:border-[1px] md: border-gray-300 rounded-3xl
-        justify-center w-[260px] h-12 font-light text-gray-800 gap-[75px]"
+        justify-center w-[260px] h-12 font-light text-white gap-[75px]" onClick={toggleOpen} 
 					>
-						<div className="flex items-center gap-3">
+						<div className="flex items-center gap-3" >
 							<AiOutlineMenu />
 							<h2>Categories</h2>
 						</div>
 						<BiChevronDown className="text-xl hidden md:block" />
 					</button>
+					<div className="absolute top-3 left-1 md:top-6 md:left-[270px]" onClick={toggleOpen} >
+					{isOpen &&<MenuCategories/>}
+					</div>
 
 					<div className="md:hidden flex gap-2">
 						<h3>Menu</h3>
@@ -92,17 +128,22 @@ const Navbar = () => {
 						</span>
 					</div>
 
-					<div className="md:pt-3 font-semibold text-gray-700 ">
+					<div className="md:pt-3 font-semibold text-white">
 						<ul className="md:flex gap-7 sm: hidden">
-							<li>Home</li>
-							<li>Features</li>
-							<li>Shortcodes</li>
-							<li>Post Types</li>
-							<li>Shop</li>
+							<li className="hover:text-red-600"><Link to='/'>Home</Link></li>
+							{/*<li className="hover:text-red-600"><Link to='/cart'>Cart</Link></li>*/}
+							<li className="hover:text-red-600"><Link to='/detail'>Detail</Link></li>
+							<li className="hover:text-red-600"><Link to='/shop'>Shop</Link></li>
+							<li className="hover:text-red-600"><Link to='/login'>Login</Link></li>
+							<li className="hover:text-red-600"><Link to='/register'>Register</Link></li>
+							<li className="hover:text-red-600"><Link to='/userprofile'>User Profile</Link></li>
 						</ul>
 					</div>
 				</div>
 			</article>
+			<div>
+			
+			</div>
 		</header>
 	)
 }
