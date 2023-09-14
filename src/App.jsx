aaf461742f5ef401897d53241504c8c1c7788b70
juanpablo1978 <jpandrujovich@gmail.com>
2023-09-14 10:01:59 -0300

    feat: codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,48 @@
-import "./App.css"
-import Home from "./views/home/Home"
-import Cart from "./views/cart/Cart"
-import Products from "./views/products/Products"
-import Footer from "./components/footer/Footer"
-import { Route, Routes } from "react-router-dom"
-import Navbar from "./components/navbar/Navbar"
-import Detail from "./views/detail/Detail"
-import Login from "./views/login/Login"
-import Register from "./views/register/Register"
-import UserDashboard from "./views/userDashboard/UserDashboard" 
-import PersonalInfo from "./components/userDashboard/personalInfo/PersonalInfo"
-import MyPurchases from "./components/userDashboard/myPurchases/MyPurchases"
-import MyProducts from "./components/userDashboard/myProducts/MyProducts"
-import AddProducts from "./components/userDashboard/addProducts/AddProducts"
-import SellsRegistry from "./components/userDashboard/sellsRegistry/SellsRegistry"
-import Orders from "./components/userDashboard/orders/Orders"
-import UserProfile from "./components/user/UserProfile"
-import Results from "./components/results/Results"
-import CardProduct from "./components/cardProduct/CardProduct"
-
-
-
-
-
+import "./App.css";
+import Home from "./views/home/Home";
+import Cart from "./views/cart/Cart";
+import Products from "./views/products/Products";
+import Footer from "./components/footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Detail from "./views/detail/Detail";
+import Login from "./views/login/Login";
+import Register from "./views/register/Register";
+import UserDashboard from "./views/userDashboard/UserDashboard";
+import PersonalInfo from "./components/userDashboard/personalInfo/PersonalInfo";
+import MyPurchases from "./components/userDashboard/myPurchases/MyPurchases";
+import MyProducts from "./components/userDashboard/myProducts/MyProducts";
+import AddProducts from "./components/userDashboard/addProducts/AddProducts";
+import SellsRegistry from "./components/userDashboard/sellsRegistry/SellsRegistry";
+import Orders from "./components/userDashboard/orders/Orders";
+import UserProfile from "./components/user/UserProfile";
+import Results from "./components/results/Results";
 
 function App() {
-
-	return (
-		<div>
-			<Navbar/>
-			<Routes>
-			<Route path='/celulares' element={<CardProduct/>}/>
-				<Route path='/' element={<Home/>}/>
-				<Route path='/cart' element={<Cart/>}/>
-				<Route path='/detail' element={<Detail/>}/>
-				<Route path='/shop' element={<Products/>}/>
-				<Route path='/login' element={<Login/>}/>
-				<Route path='/register' element={<Register/>}/>
-				<Route path='/results' element={<Results/>}/>
-				<Route path='/userprofile' element={<UserProfile/>}/>
-				<Route path='userDashboard' element={<UserDashboard/>}>
-					<Route path='personalInfo' element={<PersonalInfo />}/>
-					<Route path='mypurchases' element={<MyPurchases />}/>
-					<Route path='myproducts' element={<MyProducts />}/>
-					<Route path='addproducts' element={<AddProducts />}/>
-					<Route path='sells' element={<SellsRegistry />}/>
-					<Route path='orders' element={<Orders />}/>
-				</Route>
-			</Routes>
-      		<Footer/>
-		</div>
-	)
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/shop" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="userDashboard" element={<UserDashboard />}>
+          <Route path="personalInfo" element={<PersonalInfo />} />
+          <Route path="mypurchases" element={<MyPurchases />} />
+          <Route path="myproducts" element={<MyProducts />} />
+          <Route path="addproducts" element={<AddProducts />} />
+          <Route path="sells" element={<SellsRegistry />} />
+          <Route path="orders" element={<Orders />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-
-export default App
+export default App;
--- src/components/navbar/MenuCategories.jsx
+++ src/components/navbar/MenuCategories.jsx
@@ -7,16 +7,16 @@ const MenuCategories = () => {
      leading-10 md:w-[250px]
      border-gray-700 border-2 md:pl-4 md:leading-[50px] rounded-xl">
         <ul>      
-            <li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/'>Cameras</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/celulares'>Cell Phones</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/detail'>Computer Hardware</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/shop'>Headphones</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/login'>Laptops</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/register'>Monitors</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/userprofile'>Smart Watches</Link></li>
-            <li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/login'>Tablets</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/register'>Televisions</Link></li>
-						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/userprofile'>Video games</Link></li>
+            <li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Cameras</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Cell Phones</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Computer Hardware</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Headphones</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Laptops</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Monitors</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Smart Watches</Link></li>
+            <li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Tablets</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Televisions</Link></li>
+						<li className="hover:text-red-600 border-b-[1px] border-gray-300"><Link to='/results'>Video games</Link></li>
           </ul>
     </article>
   )
